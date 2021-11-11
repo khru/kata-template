b57aeb298a1cd646ae040c83fcbc793755f216db
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2021-11-11 07:49:37 +0100

    Update and rename PasswordValidatorTest.java to ChangeMeTest.java

--- java/src/test/java/ChangeMeTest.java
+++ java/src/test/java/ChangeMeTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class PasswordValidatorTest {
+public class ChangeMeTest {
 
 	@Test
 	public void renameMe() {
